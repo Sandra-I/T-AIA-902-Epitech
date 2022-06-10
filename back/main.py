48a09c50c48fa95df1b162bd195f924a7e38f034
Ludovic Robbe <ludovic3@icloud.com>
2022-06-10 14:48:16 +0200

    add q-learning

--- back/main.py
+++ back/main.py
@@ -1,5 +1,61 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import numpy as np
+import gym
+import random
+
+
+def run_algorithm(params):
+    env = gym.make("Taxi-v3")
+
+    action_space_size = env.action_space.n
+    state_space_size = env.observation_space.n
+    q_table = np.zeros((state_space_size, action_space_size))
+
+    nb_episodes = params.nb_episodes
+    max_steps_per_episode = params.max_steps_per_episode
+    learning_rate = params.learning_rate
+    discount_rate = params.discount_rate
+    exploration_rate = params.exploration_rate
+    max_exploration_rate = params.max_exploration_rate
+    min_exploration_rate = params.min_exploration_rate
+    exploration_decay_rate = params.exploration_decay_rate
+
+    rewards_all_episodes = []
+
+    for episode in range(nb_episodes):
+        state = env.reset()
+        rewards_current_episode = 0
+
+        for step in range(max_steps_per_episode):
+            if random.uniform(0, 1) > exploration_rate:
+                action = np.argmax(q_table[state, :])
+            else:
+                action = env.action_space.sample()
+
+            new_state, reward, done, info = env.step(action)
+
+            q_table[state, action] = q_table[state, action] * (1 - learning_rate) + learning_rate * (
+                reward + discount_rate * np.max(q_table[new_state, :]))
+            state = new_state
+            rewards_current_episode += reward
+
+            if done == True:
+                break
+        exploration_rate = min_exploration_rate + \
+            (max_exploration_rate - min_exploration_rate) * \
+            np.exp(-exploration_decay_rate*episode)
+        rewards_all_episodes.append(rewards_current_episode)
+
+    rewards_per_thousand_episodes = np.split(
+        np.array(rewards_all_episodes), nb_episodes/1000)
+
+    average_rewards = []
+    for r in rewards_per_thousand_episodes:
+        average_rewards.append(sum(r/1000))
+
+    return average_rewards
+
 
 app = FastAPI()
 
@@ -18,5 +74,5 @@ def read_root():
     return {"Hello AI World"}
 
 @app.post("/endpoint")
-def endpoint(params : Params):
-    return params
+def endpoint(params: Params):
+    return run_algorithm(params)
